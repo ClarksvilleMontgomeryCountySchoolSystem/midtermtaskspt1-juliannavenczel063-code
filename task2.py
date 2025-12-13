3651f14e615a620514feddf411545680a0d80c9c
juliannavenczel063-code <julianna.venczel063@students.cmcss.net>
2025-12-12 22:52:18 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,15 @@
+balance = starting_balance
+balance -= withdrawal_amount
 
+balance -= atm_fee
+
+total_monthly_fees = monthly_fee * months_inactive
+balance -= total_monthly_fees
+
+full_twenties = balance//20
+remaining_dollars = balance%20
+
+print(f"Account Holder: {account_holder}")
+print(f"Remaining Balance: ${balance}")
+print(f"Full $20 Bills: {full_twenties}")
+print(f"Remaining Dollars: ${remaining_dollars}")
